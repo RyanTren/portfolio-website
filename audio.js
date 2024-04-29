54c48924ad7e637fef3d178f1350faff71ce5e5c
ryan <concepting@protonmail.com>
2024-04-28 22:43:34 -0400

    a

--- audio.js
+++ audio.js
@@ -1,19 +1,19 @@
-if (window.location.href.includes("index.html")) {
-    var homeAudio = document.getElementById("homeAudio");
-    homeAudio.volume = 0.015;
-    console.log(homeAudio); // Check if homeAudio is null or undefined
-} else if (window.location.href.includes("resume.html")) {
-    var resumeAudio = document.getElementById("resumeAudio");
-    resumeAudio.volume = 0.01;
-    console.log(homeAudio); // Check if homeAudio is null or undefined
-} else if (window.location.href.includes("projects.html")) {
-    var projectAudio = document.getElementById("projectAudio");
-    projectAudio.volume = 0.02;
-    console.log(homeAudio); // Check if homeAudio is null or undefined
-} else {
-    console.log("No audio loaded!");
-<<<<<<< Updated upstream
+function playAudio() {
+    if (window.location.href.includes("index.html")) {
+        var homeAudio = document.getElementById("homeAudio");
+        homeAudio.volume = 0.015;
+        console.log(homeAudio); // Check if homeAudio is null or undefined
+    } else if (window.location.href.includes("resume.html")) {
+        var resumeAudio = document.getElementById("resumeAudio");
+        resumeAudio.volume = 0.01;
+        console.log(homeAudio); // Check if homeAudio is null or undefined
+    } else if (window.location.href.includes("projects.html")) {
+        var projectAudio = document.getElementById("projectAudio");
+        projectAudio.volume = 0.02;
+        console.log(homeAudio); // Check if homeAudio is null or undefined
+    } else {
+        console.log("No audio loaded!");
+    }
 }
-=======
-};
->>>>>>> Stashed changes
+
+
