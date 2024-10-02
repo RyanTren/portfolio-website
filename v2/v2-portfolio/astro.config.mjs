58813e1a102ef28721fbf573bee897789f283183
ryan <concepting@protonmail.com>
2024-10-01 22:55:37 -0400

    added resume

--- v2/v2-portfolio/astro.config.mjs
+++ v2/v2-portfolio/astro.config.mjs
@@ -5,7 +5,9 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({applyBaseStyles: false,})]
+  integrations: [react(), tailwind({applyBaseStyles: false,}), mdx()]
 });
