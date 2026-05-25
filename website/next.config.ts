2b89e6d0c369332351465634ce07f1e274aba3fd
RyanTren <concepting@protonmail.com>
2026-05-24 23:04:04 -0500

    [chore] nextjs 15 -> nextjs 16

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-    dirs: ["scripts"], // exclude `src/app`, `src/components`, etc.
+  turbopack: {
+    root: __dirname, // points to /website, where this config lives
   },
 };
 
